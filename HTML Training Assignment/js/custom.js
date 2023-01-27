b8f88426b55cadfb11376b8298dbbc01fc011ca3
charchil08 <er.charchilkajaliya@gmail.com>
2023-01-27 12:54:07 +0530

    sales cards done

--- HTML Training Assignment/js/custom.js
+++ HTML Training Assignment/js/custom.js
@@ -44,13 +44,82 @@ const menuItems = [
     }
 ]
 
-const sidebar = document.querySelector('.sidebar');
-console.log(sidebar);
+let salesCards = [
+    {
+        img: './images/star.svg',
+        number_of_products: 23456,
+        desc: 'New Products',
+        img_class: 'img_new img',
+    },
+    {
+        img: './images/new-product.svg',
+        number_of_products: 2345,
+        desc: 'Added Products',
+        img_class: 'img_added img',
+    },
+    {
+        img: './images/total-product.svg',
+        number_of_products: 32456,
+        desc: 'Total Products',
+        img_class: 'img_total_products img',
+    },
+    {
+        img: './images/dollar-filled.svg',
+        number_of_products: 5678,
+        desc: "Today's Sale",
+        img_class: 'img_today img',
+    },
+    {
+        img: './images/money-bag.svg',
+        number_of_products: 335342,
+        desc: 'Total Sale',
+        img_class: 'img_total_sale img'
+    }
+];
 
-for (let i = 0; i < menuItems.length; i++) {
-    let div = document.createElement('div');
-    div.innerHTML = `<img class=${menuItems[i].className} src=${menuItems[i].src} alt=${menuItems[i].alt} /> <p>${menuItems[i].text}</p>`;
-    div.classList.add('sidebar_item', `sidebar_item_${i + 1}`)
-    sidebar.append(div);
-    console.log(div);
+arrangeSidebar();
+arrangeSalesCards();
+
+function arrangeSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    console.log(sidebar);
+
+    for (let i = 0; i < menuItems.length; i++) {
+        let div = document.createElement('div');
+        div.innerHTML = `<img class=${menuItems[i].className} src=${menuItems[i].src} alt=${menuItems[i].alt} /> <p>${menuItems[i].text}</p>`;
+        div.classList.add('sidebar_item', `sidebar_item_${i + 1}`)
+        sidebar.append(div);
+    }
+}
+
+function arrangeSalesCards() {
+    // format number_of_products
+    for (let i = 0; i < salesCards.length; i++) {
+        salesCards[i].number_of_products = new Intl.NumberFormat({
+            maximumSignificantDigits: 3,
+            maximumSignificantDigits: 2,
+        }).format(salesCards[i].number_of_products);
+    }
+
+
+    const cards = document.querySelector('.sales_cards');
+
+    for (let i = 0; i < salesCards.length; i++) {
+        const { img, number_of_products, img_class, desc } = salesCards[i];
+        let div = document.createElement('div');
+        div.classList.add('card_item', `card_item_${i + 1}`);
+        div.innerHTML = `<img src=${img} class=${img_class} alt="" />`;
+
+        let div_right = document.createElement('div');
+        div_right.classList.add('div_right');
+        div_right.innerHTML = `
+        <p class='number_of_products'>${number_of_products}</p>
+        <p class='desc'>${desc}</p>`;
+
+        div.append(div_right);
+        cards.append(div);
+    }
+
+    console.log(cards);
+
 }
